3f85235b8de6201066b70a9d19acf38169d8bbf8
Majoxs <majoxs@gmail.com>
2021-08-25 17:45:57 +0500

    Problem with methods deleteItem() and completedItem()

--- js/script.js
+++ js/script.js
@@ -1,3 +1,108 @@
+/* eslint-disable indent */
 /* eslint-disable strict */
 'use strict';
 
+class Todo {
+	constructor(form, input, todoList, todoCompleted) {
+		this.form = document.querySelector(form);
+		this.input = document.querySelector(input);
+		this.todoList = document.querySelector(todoList);
+		this.todoCompleted = document.querySelector(todoCompleted);
+        this.todoData = new Map(JSON.parse(localStorage.getItem('toDoList')));
+	}
+
+    addToStorage() {
+        localStorage.setItem('toDoList', JSON.stringify([...this.todoData]));
+    }
+
+    render() {
+        this.todoList.textContent = '';
+        this.todoCompleted.textContent = '';
+        this.todoData.forEach(this.createItem, this);
+        this.addToStorage();
+    }
+
+    createItem(todo) {
+        const li = document.createElement('li');
+        li.classList.add('todo-item');
+        li.key = todo.key;
+        li.insertAdjacentHTML('beforeend', `
+            <span class="text-todo">${todo.value}</span>
+            <div class="todo-buttons">
+                <button class="todo-remove"></button>
+                <button class="todo-complete"></button>
+            </div>
+        `);
+
+        if (todo.completed) {
+            this.todoCompleted.append(li);
+        } else {
+            this.todoList.append(li);
+        }
+    }
+
+    addTodo(e) {
+        e.preventDefault();
+
+        if (this.input.value.trim() !== '') {
+            const newTodo = {
+                value: this.input.value,
+                completed: false,
+                key: this.generateKey(),
+            };
+            this.todoData.set(newTodo.key, newTodo);
+            this.input.value = '';
+            this.render();
+        } else {
+            alert('Напишите дело');
+        }
+    }
+
+    generateKey() {
+        return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    }
+
+    deleteItem() {
+        console.log('Вызвана функция Удаления');
+        this.todoData.forEach(item => {
+            console.log(item.key);
+            console.log(item);
+        });
+    }
+
+    completedItem() {
+        console.log('Вызвана функция Добавления');
+        console.log(this.todoData);
+        this.todoData.forEach(item => {
+            console.log(item);
+        });
+
+    }
+
+    handler() {
+        this.todoList.addEventListener('click', () => {
+            if (event.target.matches('.todo-complete')) {
+                this.completedItem();
+            } else if (event.target.matches('.todo-remove')) {
+                this.deleteItem();
+            }
+        });
+        this.todoCompleted.addEventListener('click', () => {
+            if (event.target.matches('.todo-complete')) {
+                this.completedItem();
+            } else if (event.target.matches('.todo-remove')) {
+                this.deleteItem();
+            }
+        });
+    }
+
+	init() {
+        this.form.addEventListener('submit', this.addTodo.bind(this));
+        this.handler();
+        this.render();
+	}
+}
+
+const todo = new Todo('.todo-control', '.header-input', '.todo-list', '.todo-completed');
+
+todo.init();
